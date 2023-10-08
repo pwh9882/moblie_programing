6ef83a7870ee915ab8dc4c76bc0e5eb16d13bff3
pwh9882 <pwh9882@gmail.com>
2023-10-09 01:54:44 +0900

    6주차: Routine.kt 수정(routineStartTime추가)

--- app/src/main/java/com/example/mobile_programing/models/Routine.kt
+++ app/src/main/java/com/example/mobile_programing/models/Routine.kt
@@ -4,6 +4,7 @@ data class Routine (
     val id: Int, // Unique ID for the routine // ID의 생성 방법: 6자리, auto increment
 
     val name: String, // Name of the routine e.g., "Leg Day"
+    val routineStartTime: Int, // Time when the routine was started
     val totalTime: Int, // total sum of time of card's timer
     val description: String?, // Optional description for the routine
     val cards: ArrayList<Card>  // List of all 'cards' that make up this routine
