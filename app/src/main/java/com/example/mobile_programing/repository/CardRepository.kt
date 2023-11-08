383c8bd339ded0bcad6759aeeb7222d21b748969
dgKim1 <kimdg3556@naver.com>
2023-11-08 16:04:22 +0900

    Make getAllCards function

--- app/src/main/java/com/example/mobile_programing/repository/CardRepository.kt
+++ app/src/main/java/com/example/mobile_programing/repository/CardRepository.kt
@@ -67,10 +67,53 @@ class CardRepository {
     }
 
     // Fetches all routines from Firestore.
-    fun getAllRoutines(): ArrayList<Card> {
-        val dummy =ArrayList<Card>()
-        return dummy
-    }
+    suspend fun getAllCards() = suspendCoroutine<ArrayList<Card>> {
+        var cards = ArrayList<Card>()
+        var continuation = it
+        cardRef.get().addOnSuccessListener {
+            it.children.forEach{
+                var card = Card("", "", "", 0, true, 0, true, 0, false, 0, arrayListOf())
+                card.id = it.child("id").value.toString()
+                card.userId = it.child("userId").value.toString()
+                card.name = it.child("name").value.toString()
+                card.preTimerSecs = Integer.parseInt(it.child("preTimerSecs").value.toString())
+                card.preTimerAutoStart =
+                    if (it.child("preTimerAutoStart").value.toString() == "true") {
+                        true
+                    } else {
+                        false
+                    }
+                card.activeTimerSecs =
+                    Integer.parseInt(it.child("activeTimerSecs").value.toString())
+                card.activeTimerAutoStart =
+                    if (it.child("activeTimerAutoStart").value.toString() == "true") {
+                        true
+                    } else {
+                        false
+                    }
+                card.postTimerSecs =
+                    Integer.parseInt(it.child("postTimerSecs").value.toString())
+                card.postTimerAutoStart =
+                    if (it.child("postTimerAutoStart").value.toString() == "true") {
+                        true
+                    } else {
+                        false
+                    }
+                card.sets = Integer.parseInt(it.child("sets").value.toString())
+                var additionalInfo = it.child("additionalInfo").value.toString()
+                additionalInfo = additionalInfo.substring(1, additionalInfo.length - 1)
+                var listAdditional = additionalInfo.split(",")
+                //카드의 aditionalInfo는 arrayList여서 반복문을 통해 list형인 listAddition의 값들을 다시 additionalInfo에 넣어줌
+                for (info: String in listAdditional) {
+                    card.additionalInfo.add(info)
+                }
+                cards.add(card)
+            }
+            continuation.resume(cards)
+
+            }
+        }
+
 
     // firebase uid로 routine 목록을 가져오는 함수
     suspend fun getCardsByUserId(userId : String) = suspendCoroutine<ArrayList<Card>> {
